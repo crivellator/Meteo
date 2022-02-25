d642c9281dc2ce5a31c67d924de14b6c8a367db1
crivellator <rosho@hotmail.it>
2022-02-25 17:24:11 +0100

    commento in environmenttest.ts

--- src/environments/environmentTest.js
+++ src/environments/environmentTest.js
@@ -2,6 +2,5 @@
 exports.__esModule = true;
 exports.environment = void 0;
 exports.environment = {
-    production: false,
-    chiaveSegreta: "934e7168d719ebdd40bc114d14bcfa6b"
+    production: false
 };

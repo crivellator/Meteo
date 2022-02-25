e64f9bea039044da1d716609b7635466c11891ea
crivellator <rosho@hotmail.it>
2022-02-25 17:10:07 +0100

    modificati environment.ts per compatibilita

--- src/environments/environment.prod.js
+++ src/environments/environment.prod.js
@@ -2,5 +2,6 @@
 exports.__esModule = true;
 exports.environment = void 0;
 exports.environment = {
-    production: true
+    production: true,
+    chiaveSegreta: "934e7168d719ebdd40bc114d14bcfa6b"
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  chiaveSegreta: "934e7168d719ebdd40bc114d14bcfa6b"
 };
